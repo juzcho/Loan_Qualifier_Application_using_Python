fa37c5fda805fb5b06f461a83e8eb09848357a37
Justine_Cho <juz317_cho@yahoo.com>
2021-06-05 21:51:38 -0700

    add save csv functionality

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -29,3 +29,26 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+# This save_csv function lets the user open the csv
+# given the location of the csv, and write a new csv data.
+def save_csv(csvpath, data, header=None): 
+    """Saves the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The CSV file path.
+        data (list of lists): A list of the rows of data for the CSV file.
+        header (list): An optional header for the CSV.
+    """
+    
+    # This will open an existing file and then it will overwrite with new data, 
+    # otherwise it'll create a new file and add new data, if file doesn't exist.
+
+    with open(csvpath, "w", newline="") as csvfile: 
+        # This will define that a comma is considered a separator 
+        # for new data in a row.
+        csvwriter = csv.writer(csvfile, delimiter=',') 
+        if header: # If a header is provided
+            csvwriter.writerow(header)  # Writes a header as the first row in the csv.
+        csvwriter.writerows(data) # Writes each item in data in each row in csv.
+    
